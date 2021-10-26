ced11f8724d1337d4280335bc1e1e4290393e805
Antoniojustino4 <32883667+Antoniojustino4@users.noreply.github.com>
2021-10-25 22:00:15 -0300

    Update TaskController.java

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ce.wcaquino.taskbackend.model.Task;
@@ -46,6 +47,7 @@ public class TaskController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		todoRepo.deleteById(id);
 	}
